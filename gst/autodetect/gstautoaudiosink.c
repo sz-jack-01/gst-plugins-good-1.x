Caesar-github/gst-plugins-good

autodetect: Add preferred for autovideosink/autoaudiosink
Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

gst/autodetect/gstautoaudiosink.c
@@ -39,6 +39,8 @@
 #include "config.h"
 #endif
 
+#include <stdlib.h>
+
 #include "gstautoaudiosink.h"
 
 #define DEFAULT_TS_OFFSET           0
@@ -95,6 +97,7 @@ gst_auto_audio_sink_init (GstAutoAudioSink * sink)
 
   autodetect->media_klass = "Audio";
   autodetect->flag = GST_ELEMENT_FLAG_SINK;
+  autodetect->preferred = getenv("AUTOAUDIOSINK_PREFERRED");
 
   sink->ts_offset = DEFAULT_TS_OFFSET;
 }

gst/autodetect/gstautovideosink.c
@@ -39,6 +39,8 @@
 #include "config.h"
 #endif
 
+#include <stdlib.h>
+
 #include "gstautovideosink.h"
 
 #define DEFAULT_TS_OFFSET           0
@@ -95,6 +97,7 @@ gst_auto_video_sink_init (GstAutoVideoSink * sink)
 
   autodetect->media_klass = "Video";
   autodetect->flag = GST_ELEMENT_FLAG_SINK;
+  autodetect->preferred = getenv("AUTOVIDEOSINK_PREFERRED");
 
   sink->ts_offset = DEFAULT_TS_OFFSET;
 }

sz-jack-01/gst-plugins-good-1.x

gst/autodetect/gstautoaudiosink.c
@@ -39,6 +39,8 @@
 #include "config.h"
 #endif
 
+#include <stdlib.h>
+
 #include "gstautodetectelements.h"
 #include "gstautodetect.h"
 #include "gstautoaudiosink.h"
@@ -99,6 +101,7 @@ gst_auto_audio_sink_init (GstAutoAudioSink * sink)
 
   autodetect->media_klass = "Audio";
   autodetect->flag = GST_ELEMENT_FLAG_SINK;
+  autodetect->preferred = g_getenv ("AUTOAUDIOSINK_PREFERRED");
 
   sink->ts_offset = DEFAULT_TS_OFFSET;
 }

gst/autodetect/gstautovideosink.c
@@ -39,6 +39,8 @@
 #include "config.h"
 #endif
 
+#include <stdlib.h>
+
 #include "gstautodetectelements.h"
 #include "gstautodetect.h"
 #include "gstautovideosink.h"
@@ -99,6 +101,7 @@ gst_auto_video_sink_init (GstAutoVideoSink * sink)
 
   autodetect->media_klass = "Video";
   autodetect->flag = GST_ELEMENT_FLAG_SINK;
+  autodetect->preferred = g_getenv ("AUTOVIDEOSINK_PREFERRED");
 
   sink->ts_offset = DEFAULT_TS_OFFSET;
 }